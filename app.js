48807007cc75f7187473b699a0385374a423b2e1
krishanleong <krishanleong@gmail.com>
2023-11-21 21:12:18 -0500

    PRoduction Mongo code

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-// const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL;
 
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp'
+// const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp'
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
